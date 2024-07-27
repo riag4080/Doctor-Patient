230c066e8f3fcf285eef67d38e8b9ed3a0b2c2a6
riya <riyagupta84715@gmail.com>
2024-07-27 20:12:52 +0530

    Initial commit

---
+++ BACKEND/database/dbConnection.js
@@ -0,0 +1,10 @@
+import mongoose from"mongoose";
+export const dbConnection =() =>{
+    mongoose.connect (process.env.MONGO_URL,{
+        dbName:"HOSPITAL-MANAGEMENT"
+    }).then(()=>{
+        console.log("connected to database!")
+    }).catch((err)=>{
+        console.log('some error occured while connecting to database: ${err}');
+    });
+};
